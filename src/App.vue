9e229b2ed2f9221a4d6b663ed4bd94f15b78465f
Annanya Sah <92743149+annanyasah7@users.noreply.github.com>
2024-12-06 11:15:32 +0100

    Expense manupulation tech. added

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,40 @@
     </footer>
   </div>
 </template>
-<script setup>
+
+<script>
+import {ref} from 'vue';
 import ExpenseForm from './components/ExpenseForm.vue';
 import ExpenseList from './components/ExpenseList.vue';
 import ExpenseSummary from './components/ExpenseSummary.vue';
+
+export default {
+  // Register components used in the app
+  components: { ExpenseForm, ExpenseList, ExpenseSummary },
+
+  // Reactive variable to store the list of expenses
+  setup() {
+    const expenses = ref([]);
+
+    //Add, Delete, Update new expenses to the list.
+    const addExpense = (expense) => {
+      expenses.value.push(expense);
+      saveToLocalStorage();
+    };
+
+    const deleteExpense = (index) => {
+      expenses.value.splice(index, 1);
+      saveToLocalStorage();
+    };
+
+    const updateExpense = (index, updatedExpense) => {
+      expenses.value[index] = updatedExpense;
+      saveToLocalStorage();
+    };
+
+    
+  },
+};
 </script>
  
 <style>
